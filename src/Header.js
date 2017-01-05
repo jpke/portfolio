fd1940d244e72d91a4e8898db307443038b31649
jpke <j.patrick.earnest@gmail.com>
2017-01-05 12:34:04 -0500

    adding code icons

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,4 @@
 import React from 'react'
-import html from '../public/html.svg'
-import css from '../public/css.svg'
-import js from '../public/js.svg'
-import R from '../public/R.png'
-import rct from '../public/react.svg'
-import rctnative from '../public/rn.svg'
-import jquery from '../public/jquery.svg'
-import node from '../public/node.svg'
-import mongodb from '../public/mongodb.svg'
-import postgresql from '../public/postgresql.png'
-
 
 const Header = () => (
   <div className="header">
