0a6ae0b0d346eac4da83917f9cdd016009cb8f83
jpke <j.patrick.earnest@gmail.com>
2017-01-05 00:49:09 -0500

    Updates

--- scroll.js
+++ scroll.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   // Smooth Scrolling Snippet from CSS Tricks
   // https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+  var _isOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
+
+  if (_isOS) {
+    $('body').addClass('is-os');
+  }
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
