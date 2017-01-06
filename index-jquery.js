784b6a92bd6a82c7d6788608d0992af048c4bc96
jpke <j.patrick.earnest@gmail.com>
2017-01-06 15:18:54 -0500

    Updates

--- index-jquery.js
+++ index-jquery.js
@@ -5,10 +5,11 @@ $(document).ready(function(){
   var _isOS = navigator.userAgent.match(/(iPod|iPhone|iPad)/);
 
   if (_isOS) {
-    $('body, "#contactAndFooter').addClass('is-os');
-    $('#projects, #contact').css("margin-top", 0);
-    $('.projectsContainer').addClass('ios-projects-layout')
-    $("body").addClass("dummyClass").removeClass("dummyClass");
+    $('body, #contactAndFooter').addClass('is-os');
+    $('#projects, #contact').css('margin-top', 0);
+    $('.projectsContainer').addClass('ios-projects-layout');
+    console.log('this ran')
+    // $("body").addClass("dummyClass").removeClass("dummyClass");
   }
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
