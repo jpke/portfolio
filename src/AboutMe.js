8f205b048c4115d741e832f4295c7c547c2533c8
jpke <j.patrick.earnest@gmail.com>
2017-01-12 19:50:35 -0500

    smaller footer photo credit font

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -9,7 +9,7 @@ const AboutMe = () => (
       <div className="aboutMeContentTechContainer">
         <section className="aboutMeContent">
           <div className="aboutMeText">
-            <p>Hi and thanks for stopping in to check out my work! I'm completing Thinkful's Fullstack Javascript Web Development Bootcamp, and am looking for opportunities to continue growing as a developer. I've built several apps showing my experience. You can check them out below. </p>
+            <p>Hi and thanks for stopping in! I'm completing Thinkful's Fullstack Javascript Web Development Bootcamp, and am looking for opportunities to continue growing as a developer. I've built several apps showcasing my experience. You can check them out below. </p>
             <p>Coding is one of my favorite activities. I love the creativity it enables and the satisfaction of writing apps into existence.</p>
             <p>Besides web development, I have a background in health and wellness. I teach pathophysiology online, dabble in data science and practice massage therapy and yoga.</p>
           </div>
