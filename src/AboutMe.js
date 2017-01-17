7c828f74e864f8566d334f32842997a7c9626089
jpke <j.patrick.earnest@gmail.com>
2017-01-16 21:06:17 -0500

    added resume

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -11,7 +11,7 @@ const AboutMe = () => (
           <div className="aboutMeText">
             <p>Hi and thanks for stopping in! I'm completing Thinkful's Fullstack Javascript Web Development Bootcamp, and am looking for opportunities to continue growing as a developer. I've built several apps showcasing my experience. You can check them out below. </p>
             <p>Coding is one of my favorite activities. I love the creativity it enables and the satisfaction of writing apps into existence.</p>
-            <p>Besides web development, I have a background in health and wellness. I teach pathophysiology online, dabble in data science and practice massage therapy and yoga.</p>
+            <p>Besides web development, I have a <a href="/JP-Earnest-Resume.pdf" target="_blank" title="Resume">background</a> in health and wellness. I teach pathophysiology online, dabble in data science and practice massage therapy and yoga.</p>
           </div>
           <div className="profilePicContainer">
             <img src={profile} className="profilePic" alt=" profile"/>
