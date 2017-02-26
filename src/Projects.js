c646381cb9d2890535f8d73659cc2c3306e1d8ed
jpke <j.patrick.earnest@gmail.com>
2017-02-26 18:30:06 -0500

    set tree as background image

--- src/Projects.js
+++ src/Projects.js
@@ -12,55 +12,60 @@ const Projects = () => (
   <div id="projects">
     <h2>Apps</h2>
     <div className="projectsContainer">
-      <Project
-        title="eLearn"
-        deployLink="https://portfolio-express.herokuapp.com/elearn/#/"
-        image={eLearn}
-        description="Online learning platform delivering course files and interactive quizzes while tracking student performance"
-        github="https://github.com/jpke/elearn"
-        technologies={["html", "css", "js", "react", "node", "mongo"]}
-      />
-      <Project
-        title="Learn Ewokese"
-        deployLink="https://jpke.github.io/spaced-rep-deploy/"
-        image={ewokese}
-        description="Using a spaced-repetition algorithm, this project teaches words used by Ewoks"
-        github="https://github.com/jpke/spaced-rep-deploy/tree/deploy"
-        technologies={["html", "css", "js", "react", "node", "mongo"]}
-      />
-      <Project
-        title="Blog App"
-        deployLink="https://appetize.io/app/jnhahxte9j2wwygntg5jummhjr?device=iphone5s&scale=75&orientation=portrait&osVersion=9.3"
-        image={blogAppReactNative}
-        description="A basic blog created with react native for iOS"
-        github="https://github.com/jpke/BlogApp"
-        technologies={["html", "css", "js", "react-native", "node", "mongo"]}
-      />
-      <Project
-        title="Medication Reminder"
-        deployLink="https://jpke.github.io/medicationReminder_Portfolio"
-        image={medicationReminder}
-        description="An app to generate email reminders to take a medication at a specific time every week"
-        github="https://github.com/jpke/medicationReminder_Portfolio"
-        technologies={["html", "css", "js", "react", "node", "mongo"]}
-      />
-      <Project
-        title="Bulldog Quiz"
-        deployLink="https://jpke.github.io/bulldogquizapp/"
-        image={bulldogquizapp}
-        description="Lighthearted interactive frontend application"
-        github="https://github.com/jpke/bulldogquizapp"
-        technologies={["html", "css", "js", "jquery"]}
-      />
-      <Project
-        title="Tic Tac Toe"
-        deployLink="https://jpke.github.io/tic-tac-toe_portfolio/"
-        image={ticTacToe}
-        description="A modular tic tac toe app, where players can play other players or the computer, and multiple games can be run simultaneously"
-        github="https://github.com/jpke/tic-tac-toe_portfolio"
-        technologies={["html", "css", "js", "react"]}
-      />
-
+      <section className="projects-row">
+        <Project
+          title="eLearn"
+          deployLink="https://portfolio-express.herokuapp.com/elearn/#/"
+          image={eLearn}
+          description="Online learning platform delivering course files and interactive quizzes while tracking student performance"
+          github="https://github.com/jpke/elearn"
+          technologies={["html", "css", "js", "react", "node", "mongo"]}
+        />
+        <Project
+          title="Learn Ewokese"
+          deployLink="https://jpke.github.io/spaced-rep-deploy/"
+          image={ewokese}
+          description="Using a spaced-repetition algorithm, this project teaches words used by Ewoks"
+          github="https://github.com/jpke/spaced-rep-deploy/tree/deploy"
+          technologies={["html", "css", "js", "react", "node", "mongo"]}
+        />
+      </section>
+      <section className="projects-row">
+        <Project
+          title="Blog App"
+          deployLink="https://appetize.io/app/jnhahxte9j2wwygntg5jummhjr?device=iphone5s&scale=75&orientation=portrait&osVersion=9.3"
+          image={blogAppReactNative}
+          description="A basic blog created with react native for iOS"
+          github="https://github.com/jpke/BlogApp"
+          technologies={["html", "css", "js", "react-native", "node", "mongo"]}
+        />
+        <Project
+          title="Medication Reminder"
+          deployLink="https://jpke.github.io/medicationReminder_Portfolio"
+          image={medicationReminder}
+          description="An app to generate email reminders to take a medication at a specific time every week"
+          github="https://github.com/jpke/medicationReminder_Portfolio"
+          technologies={["html", "css", "js", "react", "node", "mongo"]}
+        />
+      </section>
+      <section className="projects-row">
+        <Project
+          title="Bulldog Quiz"
+          deployLink="https://jpke.github.io/bulldogquizapp/"
+          image={bulldogquizapp}
+          description="Lighthearted interactive frontend application"
+          github="https://github.com/jpke/bulldogquizapp"
+          technologies={["html", "css", "js", "jquery"]}
+        />
+        <Project
+          title="Tic Tac Toe"
+          deployLink="https://jpke.github.io/tic-tac-toe_portfolio/"
+          image={ticTacToe}
+          description="A modular tic tac toe app, where players can play other players or the computer, and multiple games can be run simultaneously"
+          github="https://github.com/jpke/tic-tac-toe_portfolio"
+          technologies={["html", "css", "js", "react"]}
+        />
+      </section>
     </div>
     <div className="downArrow aboutMeArrow">
       <a href="#contact"><i className="fa fa-chevron-down" aria-hidden="true"></i></a>
--- src/Tech.js
+++ src/Tech.js
@@ -4,94 +4,94 @@ import Technologies from './Technologies'
 const Tech = () => (
     <div className="techContainer">
       <div className="tech-top-row">
-        <section className="techDiv">
-          <h2>Languages</h2>
-          <div className="flip-container" ontouchstart="this.classList.toggle('hover');">
-          	<div className="flipper">
-          		<section className="front">
-                <Technologies
-                  technologies={["html", "css", "js", "R"]}
-                  iconClass="techLearnedIcon"/>
-          		</section>
-          		<section className="back">
-                <div className="back-col">
-                  <div className="tech-back">
-                    <Technologies
-                      technologies={["html"]}
-                      iconClass="techLearnedIcon back-icon"/>
-                    <h6>HTML5</h6>
-                  </div>
-                  <div className="tech-back">
-                    <Technologies
-                      technologies={["css"]}
-                      iconClass="techLearnedIcon back-icon"/>
-                    <h6>CSS3</h6>
-                  </div>
+      <section className="techDiv">
+        <h2>Languages</h2>
+        <div className="flip-container">
+        	<div className="flipper">
+        		<section className="front">
+              <Technologies
+                technologies={["html", "css", "js", "R"]}
+                iconClass="techLearnedIcon"/>
+        		</section>
+        		<section className="back">
+              <div className="back-col">
+                <div className="tech-back">
+                  <Technologies
+                    technologies={["html"]}
+                    iconClass="techLearnedIcon back-icon"/>
+                  <h6>HTML5</h6>
                 </div>
-                <div className="back-col">
-                  <div className="tech-back">
-                    <Technologies
-                      technologies={["js"]}
-                      iconClass="techLearnedIcon back-icon"/>
-                    <h6>JavaScript/ES6</h6>
-                  </div>
-                  <div className="tech-back">
-                    <Technologies
-                      technologies={["R"]}
-                      iconClass="techLearnedIcon back-icon"/>
-                    <h6>R</h6>
-                  </div>
+                <div className="tech-back">
+                  <Technologies
+                    technologies={["css"]}
+                    iconClass="techLearnedIcon back-icon"/>
+                  <h6>CSS3</h6>
                 </div>
-          		</section>
-          	</div>
-          </div>
-        </section>
-        <section className="techDiv">
-          <h2>Frameworks</h2>
-          <div className="flip-container" ontouchstart="this.classList.toggle('hover');">
-          	<div className="flipper">
-          		<section className="front">
-                <Technologies
-                  technologies={["react", "react-native", "node", "jquery"]}
-                  iconClass="techLearnedIcon"/>
-          		</section>
-          		<section className="back">
-                <div className="back-col">
-                  <div className="tech-back">
-                    <Technologies
-                      technologies={["react"]}
-                      iconClass="techLearnedIcon back-icon"/>
-                    <h6>React</h6>
-                  </div>
-                  <div className="tech-back">
-                    <Technologies
-                      technologies={["react-native"]}
-                      iconClass="techLearnedIcon back-icon"/>
-                    <h6>React Native</h6>
-                  </div>
+              </div>
+              <div className="back-col">
+                <div className="tech-back">
+                  <Technologies
+                    technologies={["js"]}
+                    iconClass="techLearnedIcon back-icon"/>
+                  <h6>JavaScript/ES6</h6>
                 </div>
-                <div className="back-col">
-                  <div className="tech-back">
-                    <Technologies
-                      technologies={["node"]}
-                      iconClass="techLearnedIcon back-icon"/>
-                    <h6>NodeJs</h6>
-                  </div>
-                  <div className="tech-back">
-                    <Technologies
-                      technologies={["jquery"]}
-                      iconClass="techLearnedIcon back-icon"/>
-                    <h6>jQuery</h6>
-                  </div>
+                <div className="tech-back">
+                  <Technologies
+                    technologies={["R"]}
+                    iconClass="techLearnedIcon back-icon"/>
+                  <h6>R</h6>
                 </div>
-          		</section>
-          	</div>
-          </div>
-        </section>
-      </div>
+              </div>
+        		</section>
+        	</div>
+        </div>
+      </section>
+      <section className="techDiv">
+        <h2>Frameworks</h2>
+        <div className="flip-container">
+        	<div className="flipper">
+        		<section className="front">
+              <Technologies
+                technologies={["react", "react-native", "node", "jquery"]}
+                iconClass="techLearnedIcon"/>
+        		</section>
+        		<section className="back">
+              <div className="back-col">
+                <div className="tech-back">
+                  <Technologies
+                    technologies={["react"]}
+                    iconClass="techLearnedIcon back-icon"/>
+                  <h6>React</h6>
+                </div>
+                <div className="tech-back">
+                  <Technologies
+                    technologies={["react-native"]}
+                    iconClass="techLearnedIcon back-icon"/>
+                  <h6>React Native</h6>
+                </div>
+              </div>
+              <div className="back-col">
+                <div className="tech-back">
+                  <Technologies
+                    technologies={["node"]}
+                    iconClass="techLearnedIcon back-icon"/>
+                  <h6>NodeJs</h6>
+                </div>
+                <div className="tech-back">
+                  <Technologies
+                    technologies={["jquery"]}
+                    iconClass="techLearnedIcon back-icon"/>
+                  <h6>jQuery</h6>
+                </div>
+              </div>
+        		</section>
+        	</div>
+        </div>
+      </section>
+    </div>
       <section className="techDiv">
         <h2>Databases</h2>
-        <div className="flip-container" ontouchstart="this.classList.toggle('hover');">
+        <div className="flip-container">
           <div className="flipper">
             <section className="front">
               <Technologies
